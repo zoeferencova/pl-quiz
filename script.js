e4cc7fd223c9d08f91d23dc3fa9ca19520f07c4c
zoeferencova <zoeferencova@gmail.com>
2019-09-11 12:06:20 -0400

    added question and response array to js file

---
+++ script.js
@@ -0,0 +1,104 @@
+//question storage array
+const STORE = [
+	{
+		question: 'What happens to teams that finish in the bottom of the Premier League table at the end of each season?',
+		answers: [
+			'They sack their manager',
+			'They are relegated',
+			'They are given money to spend on better players',
+			'The team ceases to exist'
+		],
+		correct: 'They are relegated'
+	},
+	{
+		question: 'Which of these teams did not win a title during the first 16 seasons of the Premier League (1992-93 to 2007-08)?',
+		answers: [
+			'Liverpool',
+			'Manchester United',
+			'Blackburn Rovers',
+			'Chelsea'
+		],
+		correct: 'Liverpool'
+	},
+	{
+		question: 'Which club was the first to score 1,000 goals in the Premiership?',
+		answers: [
+			'Tottenham',
+			'Chelsea',
+			'Manchester United',
+			'Arsenal'
+		],
+		correct: 'Manchester United'
+	},
+	{
+		question: 'Which player has played for the most Premier League clubs?',
+		answers: [
+			'Rivaldo',
+			'John Burridge',
+			'Nicolas Anelka',
+			'Peter Crouch'
+		],
+		correct: 'John Burridge'
+	},
+	{
+		question: 'Which club has been promoted and relegated the most times?',
+		answers: [
+			'Birmingham City',
+			'Brighton & Hove Albion',
+			'Hull City',
+			'Leicester City'
+		],
+		correct: 'Birmingham City'
+	},
+	{
+		question: 'What is the main component of the body of the premiership trophy?',
+		answers: [
+			'Gold',
+			'Diamonds',
+			'Sterling silver',
+			'Platinum'
+		],
+		correct: 'Sterling silver'
+	},
+	{
+		question: 'How much money did Roman Abramovich buy Chelsea for in 2003?',
+		answers: [
+			'£140',
+			'£290',
+			'£100',
+			'£350'
+		],
+		correct: '£140'
+	},
+	{
+		question: 'Who has scored the most goals in the Premier League?',
+		answers: [
+			'Frank Lampard',
+			'Wayne Rooney',
+			'Andrew Cole',
+			'Alan Shearer'
+		],
+		correct: 'Alan Shearer'
+	},
+	{
+		question: 'Which two teams compete in the North London derby?',
+		answers: [
+			'Chelsea and Arsenal',
+			'Arsenal and Tottenham',
+			'Fulham and Chelsea',
+			'West Ham and Tottenham'
+		],
+		correct: 'Arsenal and Tottenham'
+	},
+	{
+		question: 'As of the 2019 summer transfer window, who became the most expensive defender in Premier League history?',
+		answers: [
+			'Virgil van Dijk',
+			'Harry Maguire',
+			'Matthijs de Ligt',
+			'Aymeric Laporte'
+		],
+		correct: 'Harry Maguire'
+	};
+];
+
